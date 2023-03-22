a275d10d184a7e51e1f46cc7489c815b32aa4d7f
Noah Miller <Noseby@live.ca>
2023-03-21 22:19:17 -0500

    dropdown finished

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,6 +8,7 @@
 
 'use strict'
 
+const icon = document.querySelector('.icon')
 const select = document.querySelector('.fa');
 const input = document.querySelector('.input');
 const file = document.querySelector('.image');
@@ -42,6 +43,7 @@ class User {
     };
 }
 
+const user = new User(4227, 'Noah Miller', 'Noseby', 'Noseby@live.ca')
 class Subscriber extends User {
     #pages;
     #groups;
@@ -57,12 +59,34 @@ class Subscriber extends User {
     get pages() { return this.#pages; }
     get groups() { return this.#groups; }
     get canMonetize() { return this.#canMonetize; }
-
-    getInfo() {
-        return `ID: ${id()}\n Name: ${name()}\n ${userName()}\n ${email()}
-        \n ${pages()}\n ${groups()}\n ${canMonetize()}`
-    };
 }
+
+const sub = new Subscriber(
+    4228, 'Andre Specht', 'AndreS', 'Andre@live.ca', 43, 2, true
+    );
+
+function getInfo() {
+    const newDiv1 = document.createElement('div');
+    newDiv1.classList.add('newDiv1');
+    
+    newDiv1.innerHTML = `
+        <div class="user">USER:</div>
+        <div class="id1">ID: ${user.id}</div>
+        <div class="name1">Name: ${user.name}</div>
+        <div class="userName1">User Name: ${user.userName}</div>
+        <div class="email1">Email: ${user.email}</div>
+        <div class="sub">SUBSCRIBER:</div>
+        <div class="id">ID: ${sub.id}</div>
+        <div class="name">Name: ${sub.name}</div>
+        <div class="userName">User Name: ${sub.userName}</div>
+        <div class="email">Email: ${sub.email}</div>
+        <div class="pages">Pages: ${sub.pages}</div>
+        <div class="groups">Groups: ${sub.groups}</div>
+        <div class="monetize">Allowed to Monetize? ${sub.canMonetize}</div>
+    `
+    out.append(newDiv1);
+};
+
 
 /* -----------------------------------*/
 /* File Input                         */
@@ -90,14 +114,31 @@ addBtn.addEventListener('click', function () {
     input.value = '';
     }, false);
 
-addBtn.addEventListener('click', addDiv);
+addBtn.addEventListener('click', newDiv);
 
-function addDiv() {
+function newDiv() {
     const newDiv = document.createElement('div');
     newDiv.classList.add('newDiv');
     out.append(newDiv);
-    let x = $('#input').val();
-    newDiv.append(x)
+    let months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let yy = date.getFullYear();
+    let year = (yy < 100) ? yy + 1900 : yy;
+    const image1 = document.createElement("IMG");
+    image1.src = "..//image/avatar.png";
+    image1.classList.add('newImg');
+    newDiv.innerHTML = `
+        <div class="divName">${image1} ${sub.name}
+        <p class="time">${months[month] + " " + day + ", " + year}</p>
+        </div>
+        
+        `;
+    
 }
 
 const selectFile = event => {
@@ -107,16 +148,4 @@ const selectFile = event => {
     }
 }
 
-let months = [
-    'January', 'February', 'March', 'April', 'May', 'June', 
-    'July', 'August', 'September', 'October', 'November', 'December'
-];
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let yy = date.getFullYear();
-let year = (yy < 100) ? yy + 1900 : yy;
 
-let timer = document.createElement('div');
-timer.innerHTML = months[month] + " " + day + ", " + year;
-document.getElementById('time').appendChild(timer);
